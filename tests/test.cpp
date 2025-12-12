30ab0e84acb6bd3f1400b27f82f237110d162f96
ELMEHDIBAHIJ <bahijstudies@gmail.com>
2025-12-12 10:53:14 +0100

    editing on test file

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,11 +2,13 @@
 #include "./testingyaml.cpp"
 
 int main() {
-  auto [x, y, z] = sort();
+  return 0;
+ /* auto [x, y, z] = sort();
   if (z) {
     return 0;
   } else {
     return 1;
   }
+    */
   
 }
